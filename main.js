3f5d4e70dea271d6d2830d6213f333e8c9660e98
Megan Huggins <Muggins1265@gmail.com>
2020-01-29 23:30:44 -0700

    Add begining of javascript code and fix html/ css

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log('Sup Girl')
+
+// find the x button
+// click the button
+// make banner dissapear
+
+// use javascript to find button and banner
+
+// create event listener to listen for the button
+//id=remove-banner
+// var closeButton = document.getElementById('removeBtn');
+//
+// closeButton.addEventListener('click', function (event) {
+//   console.log('I like to console log BOOM');
+// });
+function sayHi() {
+  var elem = document.getElementById('remove-btn');
+  elem.style.color = 'magenta';
+}
+// document.addEventListener('click', function (event) {
+//     console.log(event.target)
+//     if (event.target.matches('#remove-banner')) {
+//       console.log('huzzah!!!')
+//     }
+// })
