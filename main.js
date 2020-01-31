53de06811492331d557c2300448a6b096c4bd72a
Megan Huggins <Muggins1265@gmail.com>
2020-01-30 17:43:35 -0700

    Edit html in index.html and add more css and js to site

--- main.js
+++ main.js
@@ -4,22 +4,9 @@ console.log('Sup Girl')
 // click the button
 // make banner dissapear
 
-// use javascript to find button and banner
 
-// create event listener to listen for the button
-//id=remove-banner
-// var closeButton = document.getElementById('removeBtn');
-//
-// closeButton.addEventListener('click', function (event) {
-//   console.log('I like to console log BOOM');
-// });
-function sayHi() {
-  var elem = document.getElementById('remove-btn');
-  elem.style.color = 'magenta';
+function removeBanner() {
+  var banner = document.getElementById('remove-me');
+  console.log(banner)
+  banner.style.display = "none";
 }
-// document.addEventListener('click', function (event) {
-//     console.log(event.target)
-//     if (event.target.matches('#remove-banner')) {
-//       console.log('huzzah!!!')
-//     }
-// })
