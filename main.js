eaeaa82c74c3860ca37d05d46d7ec1194165a7f8
Megan Huggins <Muggins1265@gmail.com>
2020-02-01 13:21:34 -0700

    Add more js to first page, begin html and css second page

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ function removeBanner() {
   var banner = document.getElementById('remove-me');
   banner.style.display = "none";
 }
+
+
+
+
+  //target dashboard border, wallet, profile
+  var clipboard = document.querySelector('.clipboard');
+  var wallet = document.querySelector('.wallet');
+  console.log(wallet);
+  var profile = document.querySelector('.profile');
+  //add event listener
+  wallet.addEventListener('click', walletLight)
+  clipboard.addEventListener('click', clipboardLight)
+  profile.addEventListener('click', profileLight)
+  //if selcted element on; if not selected element off
+
+function walletLight() {
+  clipboard.classList.remove('selected')
+  wallet.classList.add('selected')
+  profile.classList.remove('selected')
+
+}
+function clipboardLight() {
+  clipboard.classList.add('selected')
+  wallet.classList.remove('selected')
+  profile.classList.remove('selected')
+}
+function profileLight() {
+  clipboard.classList.remove('selected')
+  wallet.classList.remove('selected')
+  profile.classList.add('selected')
+}
+
+
+  // if(clipboard === )
+  // e.preventDefault()
+  // e.target
+  // console.log(e.target)
+//.classList.remove look up
+//
