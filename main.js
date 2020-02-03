ed35beba5c6b3e733d125978f3efa95a702d325a
Megan Huggins <Muggins1265@gmail.com>
2020-02-02 17:00:53 -0700

    Create new html, css and jv for hover/click function for buttons on second page

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function removeBanner() {
   banner.style.display = "none";
 }
 
-
+  //move colored border in navicons
   //target dashboard border, wallet, profile
   var clipboard = document.querySelector('.clipboard');
   var wallet = document.querySelector('.wallet');
@@ -18,7 +18,7 @@ function removeBanner() {
   wallet.addEventListener('click', walletLight)
   clipboard.addEventListener('click', clipboardLight)
   profile.addEventListener('click', profileLight)
-  //if selcted element on; if not selected element off
+  //if selcted, element on; if not selected, element off
 
 function walletLight() {
   clipboard.classList.remove('selected')
@@ -37,6 +37,7 @@ function profileLight() {
   profile.classList.add('selected')
 }
 
+//switch between pages when wallet icon clicked
 //target the outter and inner main elements
 var inner = document.querySelector ('.inner');
 var outer = document.querySelector ('.outer')
@@ -51,4 +52,30 @@ function switchPage() {
 function switchPageBack() {
   outer.hidden = false
   inner.hidden = true
+}
+
+//hover and click color change page two
+//target the different transaction-button-small buttons
+var pageAll = document.querySelector ('.page-all')
+var inflow = document.querySelector ('.inflow')
+var outflow = document.querySelector ('.outflow')
+//create event listeners for each button
+pageAll.addEventListener ('click', selectPageAll)
+inflow.addEventListener ('click', selectInflow)
+outflow.addEventListener ('click', selectOutflow)
+  //create functions for if selcted, id element on; if not selected, id element off
+function selectPageAll() {
+  pageAll.id = 'blue-button'
+  inflow.id = ''
+  outflow.id = ''
+}
+function selectInflow() {
+  pageAll.id = ''
+  inflow.id ='blue-button'
+  outflow.id = ''
+}
+function selectOutflow() {
+  pageAll.id = ''
+  inflow.id = ''
+  outflow.id ='blue-button'
 }
